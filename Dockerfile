2675fc239096367dac921237eb07df63a32cc67e
Shane Riddell <shane.riddell@ellucian.com>
2014-06-14 15:29:21 -0400

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM ubuntu:14.04
+
+RUN dpkg-divert --local --rename --add /sbin/initctl
+RUN ln -sf /bin/true /sbin/initctl
+
+RUN apt-get install -y pwgen python-setuptools ejabberd ssl-cert openssl
+
+RUN /usr/bin/easy_install supervisor
+ADD ./supervisord.conf /etc/supervisord.conf
+ADD ejabberd.cfg /etc/ejabberd/ejabberd.cfg
+ADD ./start.sh /start.sh
+
+EXPOSE 5222
+EXPOSE 5280
+EXPOSE 5080
+
+CMD ["/bin/bash", "/start.sh"]
+
+
---
+++ start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DOMAIN="172.16.158.30"
+
+ADMIN_PASSWORD=`pwgen -c -n -1 12`
+echo ejabberd admin password: $ADMIN_PASSWORD
+
+if [ ! -f /etc/ssl/certificate.crt ]; then
+
+  #Add SSL cert/pem for ejabberd
+  openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=Denial/L=NA/O=Dis/CN=$DOMAIN" -keyout /etc/ejabberd/certificate.key  -out /etc/ejabberd/certificate.crt
+  cat /etc/ejabberd/certificate.crt /etc/ejabberd/certificate.key > /etc/ejabberd/ejabberd.pem
+
+  #Config for ejabberd
+  sed -i -e"s/host\.name/$DOMAIN/g" /etc/ejabberd/ejabberd.cfg
+  /usr/local/bin/supervisord
+  sleep 10s
+  ejabberdctl register admin $DOMAIN $ADMIN_PASSWORD
+fi
+/usr/local/bin/supervisord
+
+tail -f /dev/null
